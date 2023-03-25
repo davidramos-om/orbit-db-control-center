75ee900bb6b617ec001db9840fc02f7c3c6dc9b4
David Ramos <davidramos015@gmail.com>
2023-03-24 18:23:41 -0600

    apply theme to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 
 // import { Buffer } from 'buffer'
 // globalThis.Buffer = Buffer
@@ -15,10 +15,11 @@ import './index.css'
 import App from './App'
 import { AppLogProvider } from "./context/logs-reducer";
 import { AppDbProvider } from "./context/dbs-reducer";
+import theme from 'src/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AppLogProvider>
         <AppDbProvider>
       <Router>
