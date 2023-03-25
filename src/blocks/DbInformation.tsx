dacc9e1b581a8404db41397d6dfd6e775d7698fc
David Ramos <davidramos015@gmail.com>
2023-03-24 18:20:41 -0600

    add db is connected to in each page

---
+++ src/blocks/DbInformation.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Badge, Card, CardBody, CardHeader, Heading, Stack, StackDivider, Text } from "@chakra-ui/react";
+
+import { DBEntry, DBType } from "src/lib/types";
+import { Colors } from './DbList'
+
+
+type DbDetails = {
+    address: string;
+    name: string;
+    type: DBType | 'none';
+    permissions: Record<string, string>[];
+    entriesCount: number;
+}
+
+type Props = {
+    db?: DbDetails
+}
+
+export default function DbInformation({ db }: Props) {
+
+    return (
+        <Card variant={"elevated"}>
+            <CardHeader>
+                <Heading size='md'>{db?.address || '...'}</Heading>
+            </CardHeader>
+            <CardBody>
+                <Stack spacing='1'>
+                    <Text py='0.5'>
+                        Name : <b>{db?.name || '...'}</b>
+                    </Text>
+                    <Text py='0.5'>
+                        Type : <Badge
+                            variant="subtle"
+                            colorScheme={Colors[ db?.type || 'none' ] || 'yellow'}
+                        >
+                            {db?.type || 'none'}
+                        </Badge>
+                    </Text>
+                    <Text py='0.5'>
+                        Entries : <b> {db?.entriesCount || '0'}</b>
+                    </Text>
+                </Stack>
+            </CardBody>
+        </Card>
+    );
+}
---
+++ src/components/DbGoBack.tsx
@@ -0,0 +1,30 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Heading, HStack, IconButton } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+import { PATH } from "../routes";
+
+export default function DbGoBack() {
+
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(PATH.HOME);
+    }
+
+    return (
+        <HStack>
+            <IconButton
+                aria-label="Go back"
+                onClick={handleGoBack}
+            >
+                <ArrowBackIcon />
+            </IconButton>
+            <Heading
+                size="md"
+            >
+                DATABASE
+            </Heading>
+        </HStack>
+    );
+}
