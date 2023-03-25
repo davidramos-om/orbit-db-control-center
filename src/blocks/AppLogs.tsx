0872af292164299a4e25a8d6ad2cb627e58929f5
David Ramos <davidramos015@gmail.com>
2023-03-24 18:20:18 -0600

    show logs

--- src/blocks/AppLogs.tsx
+++ src/blocks/AppLogs.tsx
@@ -1,8 +1,27 @@
 import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel, useColorModeValue, HStack, Text } from '@chakra-ui/react'
+
 import { TreeDotsHorizontalSvgIcon } from "src/components/TreeDotsHorizontalSvgIcon";
+import { useAppLog } from "src/context/logs-reducer";
+
+
+type Colors = {
+    [ key: string ]: string
+}
+
+export const Colors: Colors = {
+    'created': 'green',
+    'updated': 'blue',
+    'deleted': 'red',
+    'synced': 'purple',
+    'pinned': 'orange',
+    'preview': 'gray',
+    'none': 'yellow'
+}
+
 
 export default function AppLogs() {
 
+    const { logs } = useAppLog();
     const bg = useColorModeValue('gray.300', 'blackAlpha.900');
     const itemsBg = useColorModeValue('gray.100', 'gray.900');
     const titleColor = useColorModeValue('gray.900', 'gray.100');
@@ -32,7 +51,23 @@ export default function AppLogs() {
                     pb={4}
                     bg={itemsBg}
                 >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {logs.map((log) => (
+                        <Box key={log.id}>
+                            <Text
+                                as="b"
+                                colorScheme={Colors[ log.type ]}
+                            >
+                                {log.type}
+                            </Text>
+                            <Text>
+                                {log.text}
+                            </Text>
+                            <Text>
+                                {log.text}
+                            </Text>
+                        </Box>
+
+                    ))}
                 </AccordionPanel>
             </AccordionItem>
         </Accordion>
