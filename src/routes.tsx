1566d2fbbb7358ef28c770c26306fa129f59bfaf
David Ramos <davidramos015@gmail.com>
2023-03-24 18:21:44 -0600

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,28 +3,69 @@ import { RouteObject } from 'react-router-dom';
 
 
 import HomePage from './pages';
-
-// export const HomePage = lazy(() => import('./pages/index'))
 export const AboutPage = lazy(() => import('./pages/about'))
 export const BlogPage = lazy(() => import('./pages/blog'))
 export const NotFoundPage = lazy(() => import('./pages/404'))
 
+export const CounterDbPage = lazy(() => import('./pages/dbs/counter'));
+export const DocumentDbPage = lazy(() => import('./pages/dbs/document'));
+export const EventLogDbPage = lazy(() => import('./pages/dbs/eventlog'));
+export const FeedDbPage = lazy(() => import('./pages/dbs/feed'));
+export const KeyValueDbPage = lazy(() => import('./pages/dbs/keyvalue'));
+
+export const PATH = {
+    HOME: '/',
+    ABOUT: '/about',
+    BLOG: '/blog',
+    DB: {
+        COUNTER: '/db/counter/:id/:name',
+        DOCSTORE: '/db/document/:id/:name',
+        EVENTLOG: '/db/eventlog/:id/:name',
+        FEED: '/db/feed/:id/:name',
+        KEYVALUE: '/db/keyvalue/:id/:name',
+    },
+}
 
 export const routes: RouteObject[] = [
     {
-        path: '/',
+        path: PATH.HOME,
         index: true,
         element: <HomePage />,
     },
     {
-        path: 'about',
+        path: PATH.ABOUT,
         index: true,
         element: <AboutPage />,
     },
     {
-        path: 'blog',
+        path: PATH.BLOG,
         index: true,
         element: <BlogPage />,
+    },
+    {
+        path: PATH.DB.COUNTER,
+        index: true,
+        element: <CounterDbPage />,
+    },
+    {
+        path: PATH.DB.DOCSTORE,
+        index: true,
+        element: <DocumentDbPage />,
+    },
+    {
+        path: PATH.DB.EVENTLOG,
+        index: true,
+        element: <EventLogDbPage />,
+    },
+    {
+        path: PATH.DB.FEED,
+        index: true,
+        element: <FeedDbPage />,
+    },
+    {
+        path: PATH.DB.KEYVALUE,
+        index: true,
+        element: <KeyValueDbPage />,
     },
     {
         path: '*',
