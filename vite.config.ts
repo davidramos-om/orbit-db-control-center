6afd3aae837402c5aa906fdc3957ee7e6a8b0031
David Ramos <davidramos015@gmail.com>
2023-03-28 18:40:20 -0600

    cleanup : package sand dead code

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Pages from 'vite-plugin-pages'
 import eslint from 'vite-plugin-eslint';
-import Inject from '@rollup/plugin-inject';
-// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-// import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-// import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-// yarn add --dev @esbuild-plugins/node-modules-polyfill
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-
 
 //! NOTES : ABOUT POLYFILL fs, process, buffer for orbit-db and ipfs
 //* - nodePolyfills from "vite-plugin-node-stdlib-browser" works well for development, but not for build
@@ -20,66 +12,14 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 //* - it looks like is not needed to set up the esbuildOptions for optimizeDeps
 //* - it looks like is {protocolImports: true} is not needed.
 
-
-
 //! https://vitejs.dev/config/
 
 export default defineConfig({ 
-  // build: {
-  //   target: 'es2020',
-  //   minify: false,
-  //   commonjsOptions: {
-  //     include: []
-  //   },
-  //   rollupOptions: {
-  //     plugins: [ rollupNodePolyFill() ],
-  //   }
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     // Node.js global to browser globalThis
-  //     define: {
-  //       global: 'globalThis'
-  //     },
-  //     // Enable esbuild polyfill plugins
-  //     plugins: [
-  //       NodeGlobalsPolyfillPlugin({
-  //         process: true,
-  //         buffer: true
-  //       }),
-  //     ]
-  //   }
-  // },
   resolve: {
     alias: {
       '~/': path.resolve(__dirname, './src/*'),
-      // fs: 'fs-browserify',
-      // stream: "stream-browserify",
-      // path: 'rollup-plugin-node-polyfills/polyfills/path',
     }
   },
-  // define: {
-  //   'process.env.NODE_DEBUG': 'false',
-  //   'global': 'globalThis',
-  //   'Buffer': 'globalThis.Buffer',
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     // Node.js global to browser globalThis
-  //     define: {
-  //       'process.env.NODE_DEBUG': 'false',
-  //       global: 'globalThis',
-  //     },
-  //     // Enable esbuild polyfill plugins
-  //     plugins: [
-  //       NodeGlobalsPolyfillPlugin({
-  //         buffer: true,
-  //         process: true,
-  //       }),
-  //       // NodeModulesPolyfillPlugin(),
-  //     ],
-  //   },
-  // },
   plugins: [
     react({
       include: "**/*.tsx"
