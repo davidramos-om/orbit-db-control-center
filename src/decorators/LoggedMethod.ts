0ebcbc73bb4063751e5d9d0379caafa31d415b54
David Ramos <davidramos015@gmail.com>
2023-04-01 15:10:54 -0600

    test decorator with TS 5

---
+++ src/decorators/LoggedMethod.ts
@@ -0,0 +1,15 @@
+export function loggedMethod<This, Args extends any[], Return>(
+    target: (this: This, ...args: Args) => Return,
+    context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>
+) {
+    const methodName = String(context.name);
+
+    function replacementMethod(this: This, ...args: Args): Return {
+        console.log(`LOG: Entering method '${methodName}'.`)
+        const result = target.call(this, ...args);
+        console.log(`LOG: Exiting method '${methodName}'.`)
+        return result;
+    }
+
+    return replacementMethod;
+}
