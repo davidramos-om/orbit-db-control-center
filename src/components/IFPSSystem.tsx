49f3b2c3851f3af4be5b6a8fb7074315ad314db8
David Ramos <davidramos015@gmail.com>
2023-04-23 15:19:40 -0600

    feat: watch ipfs status changes

--- src/components/IFPSSystem.tsx
+++ src/components/IFPSSystem.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useToken } from "@chakra-ui/react";
-import { type IPFS } from "ipfs-core";
+import { type IPFS, } from "ipfs-core";
 
 import { useAppLogDispatch } from "#/context/logs-reducer";
 import useIsMounted from "#/hooks/useIsMounted";
@@ -16,8 +16,11 @@ type Props = {
 export function IFPSSystem({ onIpfsReady }: Props) {
 
     const dispatch = useAppLogDispatch();
-    const isMounted = useIsMounted();;
+    const isMounted = useIsMounted();
+
     const [ dbState, setDbState ] = useState<SystemState>(SystemState.connecting);
+    const [ ipfs, setIpfs ] = useState<IPFS | null>(null);
+
     const color = useToken('colors', dbState === SystemState.connected ? 'green.500' : dbState === SystemState.error ? 'red.500' : dbState === SystemState.disconnected ? 'black.500' : 'orange.500');
 
     useEffect(() => {
@@ -33,13 +36,17 @@ export function IFPSSystem({ onIpfsReady }: Props) {
                     }
                 });
 
-                const ipfs = await initIPFS() as any;
-
+                const ipfs = await initIPFS();
                 if (!isMounted())
                     return;
 
-                if (Object.keys(ipfs).length > 0) {
+                if (ipfs) {
+
                     setDbState(() => SystemState.connected);
+                    setIpfs(() => ipfs);
+                    if (!isMounted())
+                        return;
+
                     onIpfsReady(ipfs);
 
                     dispatch({
@@ -69,6 +76,30 @@ export function IFPSSystem({ onIpfsReady }: Props) {
         setTimeout(() => { init(); }, 1500);
 
     }, [ dispatch, isMounted, onIpfsReady ]);
+
+
+    useEffect(() => {
+
+        if (!ipfs)
+            return;
+
+        const check = async () => {
+
+            try {
+                await ipfs.id();
+                setDbState(() => SystemState.connected);
+            }
+            catch (error: any) {
+                setDbState(() => SystemState.disconnected);
+            }
+        };
+
+        const interval = setInterval(() => { check(); }, 5000);
+
+        return () => { clearInterval(interval); };
+
+    }, [ ipfs ]);
+
 
     return (
         <SytemStatusIcon label="IPFS" color={color} />
