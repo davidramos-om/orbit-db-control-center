09e2cf5b3f228eb96e3c6ec546617dcb38720ebf
David Ramos <davidramos015@gmail.com>
2023-04-23 23:33:19 -0600

    feat: show ipfs and orbit-db information

---
+++ src/components/IFPSSystemInfo.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useRef, useState } from 'react';
+import { List, ListItem, AlertDialog, AlertDialogOverlay, AlertDialogCloseButton, AlertDialogHeader, AlertDialogContent, AlertDialogBody, Button } from "@chakra-ui/react";
+import { type IPFS, } from "ipfs-core";
+
+type Props = {
+    open: boolean;
+    ipfs: IPFS | null;
+    onClose: () => void;
+};
+
+export function IFPSSystemInfo({ open, ipfs, onClose }: Props) {
+
+    const cancelRef = useRef();
+    const [ info, setInfo ] = useState<{
+        id: string;
+        version: string;
+        agentVersion: string;
+        protocolVersion: string;
+        repo: string,
+        online: boolean,
+    }>({
+        id: '',
+        version: '',
+        agentVersion: '',
+        protocolVersion: '',
+        repo: '',
+        online: false,
+    });
+
+    useEffect(() => {
+
+        if (!ipfs)
+            return;
+
+        const getInfo = async () => {
+
+            const repo = await ipfs.repo.stat();
+            const id = await ipfs.id();
+            const version = await ipfs.version();
+            const agentVersion = id.agentVersion;
+            const protocolVersion = id.protocolVersion;
+            const online = ipfs.isOnline();
+
+            setInfo({
+                id: id.publicKey,
+                online: online,
+                version: version.version,
+                agentVersion: agentVersion,
+                protocolVersion: protocolVersion,
+                repo: repo.repoPath,
+            });
+
+        }
+
+        getInfo();
+
+    }, [ ipfs ]);
+
+    if (!ipfs)
+        return null;
+
+    return (
+        <AlertDialog
+            isOpen={open}
+            motionPreset='slideInBottom'
+            leastDestructiveRef={cancelRef as any}
+            onClose={onClose}
+        >
+            <AlertDialogOverlay />
+            <AlertDialogContent
+            >
+                <AlertDialogCloseButton />
+                <AlertDialogHeader>
+                    IPFS Info
+                </AlertDialogHeader>
+                <AlertDialogBody>
+                    <List spacing={3}>
+                        <ListItem>
+                            <b>Online:</b> {info.online ? 'Yes' : 'No'}
+                        </ListItem>
+                        <ListItem>
+                            <b>Public key:</b> {info.id}
+                        </ListItem>
+                        <ListItem>
+                            <b>Version:</b> {info.version}
+                        </ListItem>
+                        <ListItem>
+                            <b>agent Version:</b> {info.agentVersion}
+                        </ListItem>
+                        <ListItem>
+                            <b>Protocol Version:</b> {info.protocolVersion}
+                        </ListItem>
+                        <ListItem>
+                            <Button
+                                width="100%"
+                                variant="ghost"
+                                colorScheme="blue"
+                                onClick={() => {
+                                    console.info('IFPSSystemInfo.ipfs', ipfs);
+                                }}
+                            >
+                                Print to console
+                            </Button>
+                        </ListItem>
+                    </List>
+                </AlertDialogBody>
+            </AlertDialogContent>
+
+        </AlertDialog>
+    );
+}
---
+++ src/components/OrbitDbSystemInfo.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from 'react';
+import { List, ListItem, Button, AlertDialog, AlertDialogOverlay, AlertDialogCloseButton, AlertDialogHeader, AlertDialogContent, AlertDialogBody } from "@chakra-ui/react";
+import type OrbitDB from "orbit-db";
+
+type Props = {
+    open: boolean;
+    orbitDb: OrbitDB | null;
+    onClose: () => void;
+};
+
+export function OrbitDbSystemInfo({ open, orbitDb, onClose }: Props) {
+
+    const cancelRef = useRef();
+    const [ info, setInfo ] = useState<{
+        id: string;
+        identityId: string;
+        directory: string;
+        publicKey: string;
+    }>({
+        id: '',
+        identityId: '',
+        publicKey: '',
+        directory: '',
+    });
+
+    useEffect(() => {
+
+        if (!orbitDb)
+            return;
+
+        const getInfo = async () => {
+
+            const dbAny = orbitDb as any;
+            setInfo({
+                id: dbAny.id || '',
+                identityId: dbAny.identity.id || '',
+                publicKey: dbAny.identity.publicKey || '',
+                directory: dbAny.directory || '',
+            });
+
+        }
+
+        getInfo();
+
+    }, [ orbitDb ]);
+
+    if (!orbitDb)
+        return null;
+
+    return (
+        <AlertDialog
+            isOpen={open}
+            motionPreset='slideInBottom'
+            leastDestructiveRef={cancelRef as any}
+            onClose={onClose}
+        >
+            <AlertDialogOverlay />
+            <AlertDialogContent
+            >
+                <AlertDialogCloseButton />
+                <AlertDialogHeader>
+                    OrbitDB Info
+                </AlertDialogHeader>
+                <AlertDialogBody>
+                    <List spacing={3}>
+
+                        <ListItem>
+                            <b>Id:</b> {info.id}
+                        </ListItem>
+                        <ListItem>
+                            <b>Identity Id:</b> {info.identityId}
+                        </ListItem>
+                        <ListItem>
+                            <b>Public Key:</b> {info.publicKey}
+                        </ListItem>
+                        <ListItem>
+                            <b>Directory:</b> {info.directory}
+                        </ListItem>
+                        <ListItem>
+                            <Button
+                                width="100%"
+                                variant="ghost"
+                                colorScheme="blue"
+                                onClick={() => {
+                                    console.info('OrbitDbSystemInfo.orbit-db', orbitDb);
+                                }}
+                            >
+                                Print to console
+                            </Button>
+                        </ListItem>
+                    </List>
+                </AlertDialogBody>
+            </AlertDialogContent>
+
+        </AlertDialog>
+    );
+}
