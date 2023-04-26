b86c31913735a1cf900d64d017f5b46e1aed42c5
David Ramos <davidramos015@gmail.com>
2023-04-25 23:16:11 -0600

    feat: add more multiaddresses

--- src/lib/ipfs-config.ts
+++ src/lib/ipfs-config.ts
@@ -24,8 +24,8 @@ const ipfsConfig: Options = {
     Addresses: {
       Swarm: [
         '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-        // '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-        // '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/'
       ]
     }
   }
